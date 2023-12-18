bbcc5b05bf6aba2a538b3197ce44485b28ddcbd4
Canelle Wagner <144630318+Canelle-tech@users.noreply.github.com>
2023-12-17 21:56:20 -0500

    Docker Partie 1 - Service de Prédiction

--- docker-project-milestone3/build.sh
+++ docker-project-milestone3/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "TODO: fill in the docker build command"
+# echo "TODO: fill in the docker build command"
+
+echo "Construction de l'image Docker pour l'application Flask en cours..."
+
+docker build -t ift6758/serving:1.0.0 .
+
+# Afficher un message de succès
+echo "L'image Docker 'flask-app' a été construite avec succès."#!/bin/bash
+
--- docker-project-milestone3/run.sh
+++ docker-project-milestone3/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "TODO: fill in the docker run command"
+
+echo "Démarrage de l'application Flask dans le conteneur Docker..."
+
+docker run -d -p 8000:8000 --env COMET_API_KEY=$COMET_API_KEY ift6758/serving:1.0.0
+
