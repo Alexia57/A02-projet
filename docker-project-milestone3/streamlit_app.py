1aedab10e7b4c704081acebe6e8a85a3d505b050
Canelle Wagner <144630318+Canelle-tech@users.noreply.github.com>
2023-12-18 21:41:57 -0500

    modification streamlit_app.py

--- docker-project-milestone3/streamlit_app.py
+++ docker-project-milestone3/streamlit_app.py
@@ -2,8 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import json
-from ift6758.ift6758.client.game_client import ServingClient
-from ift6758.ift6758.client.game_client import GameClient
+import sys
+#sys.path.append('./ift6758/ift6758/')
+
+#from ift6758.ift6758.client import serving_client
+#from ift6758.ift6758.client import game_client
+#from client.serving_client import ServingClient
+#from client.game_client import GameClient
 
 """
 General template for your streamlit app. 
@@ -11,21 +16,18 @@ Feel free to experiment with layout and adding functionality!
 Just make sure that the required functionality is included as well
 """
 
-st.title("NHL data visualisation API")
+st.title("Hockey Visualisation APP")
 
-sc = ServingClient.ServingClient()
-gc = GameClient.GameClient()
+#sc = ServingClient.ServingClient()
+#gc = GameClient.GameClient()
 
 
 with st.sidebar:
-    st.header("Workspace")
-    workspace = st.selectbox("Select a workspace", ["a02-ift6758-milestone-2"])
+    workspace = st.selectbox("Workspace", ["a02-ift6758-milestone-2"])
 
-    st.header("Model")
-    model = st.selectbox("Select a model", ["model_distance", "model_distance_angle"])
+    model = st.selectbox("Model",["model_distance_angle"])
 
-    st.header("Version")
-    version = st.selectbox("Select a version", ["Choisissez une option", "1.0.0"])
+    version = st.selectbox("Version",["", "1.0.0"])
 
     if st.button('Download Model'):
         print('workspace: ', workspace)
@@ -40,7 +42,7 @@ with st.sidebar:
 
 with st.container():
     # TODO: Add Game ID input
-    #game_id = st.text_input("Select a match", 2022030411)
+    game_id = st.text_input("Game ID",2022030411)
     pass
 
 with st.container():
